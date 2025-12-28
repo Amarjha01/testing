fb4b2ab973b9a1b663a1a8cc13599030b9f6a65d
Amar <amarjha880@gmail.com>
2025-12-28 15:34:42 +0530

    now port listen from anyware add 0.0.0.0

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ res.status(200).json({
 })
 })
 
-app.listen(5000 , ()=>{
-    console.log("app is up and running at port 5000");
-})
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
+});
